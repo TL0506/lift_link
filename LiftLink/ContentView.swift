b574d9a098e06f16390f1d533109a3d550cb5e3f
Tiffany Lin <Tiffanylin511@gmail.com>
2024-03-13 20:19:26 -0400

    Initial Commit

---
+++ LiftLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiftLink
+//
+//  Created by Tiffany Lin on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiftLink/LiftLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftLinkApp.swift
+//  LiftLink
+//
+//  Created by Tiffany Lin on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct LiftLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

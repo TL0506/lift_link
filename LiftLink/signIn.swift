eb72cfeb5e7c625e857a4bed26ec358c0ca912d8
cow <119525679+TL0506@users.noreply.github.com>
2024-04-02 20:42:55 -0400

    send help

---
+++ LiftLink/signIn.swift
@@ -0,0 +1,18 @@
+//
+//  SignIn.swift
+//  LiftLink
+//
+//  Created by Tiffany Lin on 4/2/24.
+//
+
+import SwiftUI
+
+struct SignIn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignIn()
+}

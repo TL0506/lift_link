6f7895d60db4d4583752c69a46088e3758a01793
cow <119525679+TL0506@users.noreply.github.com>
2024-04-04 19:50:38 -0400

    failed

---
+++ LiftLink/Chat.swift
@@ -0,0 +1,18 @@
+//
+//  Chat.swift
+//  LiftLink
+//
+//  Created by Tiffany Lin on 4/4/24.
+//
+
+import SwiftUI
+
+struct Chat: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chat()
+}
---
+++ LiftLink/DataStorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataStorageManager.swift
+//  LiftLink
+//
+//  Created by Tiffany Lin on 4/4/24.
+//
+
+import Foundation

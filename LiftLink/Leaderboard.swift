beb403228e38c1529233b10451be1a8491631f58
cow <119525679+TL0506@users.noreply.github.com>
2024-04-07 11:50:32 -0400

    work

---
+++ LiftLink/Leaderboard.swift
@@ -0,0 +1,18 @@
+//
+//  Leaderboard.swift
+//  LiftLink
+//
+//  Created by Tiffany Lin on 4/7/24.
+//
+
+import SwiftUI
+
+struct Leaderboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Leaderboard()
+}
---
+++ LiftLink/Map.swift
@@ -0,0 +1,18 @@
+//
+//  Map.swift
+//  LiftLink
+//
+//  Created by Tiffany Lin on 4/7/24.
+//
+
+import SwiftUI
+
+struct Map: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Map()
+}

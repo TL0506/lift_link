452c6c44d8e66944e2e0246e23d5f2be0d960b6d
cow <119525679+TL0506@users.noreply.github.com>
2024-04-07 11:50:47 -0400

    woork

--- LiftLink/Chat.swift
+++ LiftLink/Chat.swift
@@ -39,7 +39,7 @@ struct Chat: View {
            }
        }
    }
-    struct ContentView_Previews: PreviewProvider{
+struct Chat_Previews: PreviewProvider{
         static var previews: some View{
             Chat()
         }
--- LiftLink/ContentView.swift
+++ LiftLink/ContentView.swift
@@ -9,27 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
-            
-            ZStack{
-                
+        
+        NavigationStack {
+
+            ZStack {
                 Image(.homepage)
-                    .resizable()
-                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .resizable(resizingMode: .stretch)
                     .ignoresSafeArea()
                 
-            
-                NavigationLink(destination:SignIn()){
-                    Image(.pfp)
-                        .resizable()
-                        .edgesIgnoringSafeArea(.top)
-                        .frame(width: 60, height: 60)
+                VStack {
+                    Spacer()
+                    HStack {
+                        Image(.homeicon)
+                                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                                .frame(width: 100,
+                                     height: 100)
+                    
+                        Spacer()
+                        NavigationLink(destination: Chat()) {
+                            Image(.chaticon)
+                                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                                .frame(width: 100, height: 100)
+                        }
+                        Spacer()
+                        NavigationLink(destination: Leaderboard()) {
+                            Image(.leaderboard)
+                                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                                .frame(width: 80.0, height: 70.0)
+                        }
+                        Spacer()
+                        NavigationLink(destination: Map()) {
+                            Image(.mapicon)
+                                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                                .frame(width: 75.0, height: 50.0)
+                        }
+                        Spacer()
+                        NavigationLink(destination: SignIn()) {
+                            Image(.pfp)
+                                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                                .frame(width: 50.0, height: 50.0)
+                            
+                        }
+                        Spacer()
+                    }
+                    .padding(.top)
+                    .padding(.leading)
+
+
+
                 }
             }
+            
         }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }

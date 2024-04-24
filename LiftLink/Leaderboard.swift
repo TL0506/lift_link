a88b9dd8bf2c6fdfee0a8b4db5698312f959d29e
Selina Wang <wselina07@gmail.com>
2024-04-24 17:27:31 -0400

    baka  vibess

--- LiftLink/Leaderboard.swift
+++ LiftLink/Leaderboard.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct Leaderboard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(.lee)
+            .resizable(resizingMode: .stretch)
+            .ignoresSafeArea()
     }
 }
 

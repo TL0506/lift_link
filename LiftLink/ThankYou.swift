84edb7eb52839b3f1054e9ac25ebfbc8e587a6e4
Selina Wang <wselina07@gmail.com>
2024-04-24 14:49:00 -0400

    ihiohdiqwhiodhqwihdqiwdhqwd

---
+++ LiftLink/ThankYou.swift
@@ -0,0 +1,18 @@
+//
+//  ThankYou.swift
+//  LiftLink
+//
+//  Created by Wang, Selina on 4/24/24.
+//
+
+import SwiftUI
+
+struct ThankYou: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThankYou()
+}
